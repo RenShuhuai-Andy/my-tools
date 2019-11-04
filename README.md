09d46019c0d90b750f84f0bc6025295922e5de63
RenShuhuai-Andy <739584819@qq.com>
2019-11-04 16:45:37 +0800

    add new shell script

---
+++ README.md
@@ -0,0 +1,4 @@
+# My Tools
+
+自己造的一些轮子
+- `install_package.sh`：使用conda命令安装包
---
+++ shell script/install_package.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#!/usr/bin/php
+# 使用conda命令安装包，在conda install不可用的情况下，使用pip
+# 或者conda install --yes --file requirements.txt
+while read requirement; do conda install --yes $requirement || pip install $requirement; done < requirements.txt
