00f0e77cea67f239d6bb6d6d74fa33b5f1715550
739584819@qq.com <739584819@qq.com>
2019-11-19 13:42:33 +0800

    add get_batch.py

---
+++ python script/get_batch.py
@@ -0,0 +1,26 @@
+import math
+import random
+import numpy as np
+
+def get_batch_data(inputs, batch_size=None, shuffle=False):
+    rows = len(inputs)
+    indices = list(range(rows))
+    if shuffle:
+        random.seed(100)
+        random.shuffle(indices)
+    i = 0  # TODO better solution?
+    while i <= math.ceil(rows / batch_size) - 1:  # TODO need -1?
+        batch_indices = np.asarray(indices[0:batch_size])  # 产生一个batch的index
+        indices = indices[batch_size:] + indices[:batch_size]  # 循环移位，以便产生下一个batch
+
+        data = np.asarray(inputs)
+        batch_data = data[batch_indices].tolist()  # 使用下标查找，必须是ndarray类型类型
+        i += 1
+        yield batch_data
+
+inputs = 
+batch_size = 
+batches = get_batch_data(inputs, batch_size=batch_size)
+
+for batch in batches:
+    
